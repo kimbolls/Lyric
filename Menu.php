a16ebfb39596cc15f62b2ed265c0625ab0de89c8
kimbolls <42145771+kimbolls@users.noreply.github.com>
2020-01-07 20:54:57 +0800

    Create Menu.php

---
+++ Menu.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title> Home Page </title>
+</head>
+
+<body>
+<center>
+<h1> Welcome to Hi-Fi! </h1>
+<form action="login.php" method="post">
+    Username<br>
+    <input type="text" name="UserName" placeholder="Enter Username" required><br><br>
+    Password<br>
+    <input type="password" name="UserPwd" placeholder="Enter Password" required><br><br>
+    <input type="submit" value="LOGIN">
+</form>
+</center>
+</html>

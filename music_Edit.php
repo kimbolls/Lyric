33659adf82eafd7e354e4d9f3a2e6a42e21e05b2
Nik Asyraf <54460063+NikAsyraf454@users.noreply.github.com>
2020-01-08 13:13:00 +0800

    Create music_Edit.php
    new file

---
+++ music_Edit.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head><title>Song Update</title></head>
+
+<body>
+<h3>Song Edit Details</h3>
+
+<?php
+
+$albumname = $_POST['albumname'];
+$songname = $_POST['songname'];
+$artistname = $_POST['artistname'];
+$featartistname = $_POST['featartistname'];
+$songgenre = $_POST['songgenre'];
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "hi-fi";
+
+$link = mysqli_connect($host,$user,$password,$database);
+
+if(!$link)
+{
+	die ("Could not connect to database".mysqli_connect_error($link));
+}
+else
+{
+	$queryUpdate = "UPDATE MUSIC SET Song_Name = '".$songname."', Artist_Name = '".$artistname."', Featuring_Artist_Name = '".$featartistname."', Song_Genre =  '".$songgenre."' WHERE Album_Name = '".$albumname."' "; 
+	
+	$resultUpdate = mysqli_query($link, $queryUpdate);
+	
+	if(!$resultUpdate)
+	{
+		die ("Query problematic: ". mysqli_error($link)); 
+	}
+	else
+	{
+		echo "Record has been updated into database";
+		echo "<br><br>";
+		echo "Click <a href='music_view.php'> here </a> to view pet list";
+	}
+}
+?>
+
+
+
+</body>
+</html>

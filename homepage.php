79d1f5db5f1983a5e733770232826bf96710cbd7
kimbolls <42145771+kimbolls@users.noreply.github.com>
2020-01-08 21:01:53 +0800

    Navigation push to right
    implemented to homepage.php

--- homepage.php
+++ homepage.php
@@ -12,22 +12,30 @@ if(isset($_SESSION["UserID"])){
 <title> Hi-Fi - Music Registration </title>
 <body>
 
-<div class="w3-sidebar w3-bar-block w3-animate-left" style="display:none;z-index:5" id="mySidebar">
-  <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
+<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
+  <button class="w3-bar-item w3-button w3-large"
+  onclick="w3_close()">Close &times;</button>
   <a href="music_insert.html" class="w3-bar-item w3-button"> Register New Music </a>
   <a href="music_view.php" class="w3-bar-item w3-button"> View Music </a>
-  <a href="music_view.php" class="w3-bar-item w3-button" > View Music </a>
   <a href="music_EditView.php" class="w3-bar-item w3-button"> Edit Music </a> 
   <a href="music_DeleteView.php" class="w3-bar-item w3-button"> Delete Music </a>
+  <a href="logout.php" class="w3-bar-item w3-button">Logout </a><br>
 </div>
 
-<div class="w3-overlay w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" id="myOverlay"></div>
-<div>
-  <button class="w3-button w3-white w3-xxlarge" onclick="w3_open()">&#9776;</button>
+<div id="main">
+
+<div class="w3-teal">
+  <button id="openNav" class="w3-button w3-teal w3-xlarge" onclick="w3_open()">&#9776;</button>
   <div class="w3-container">
-  <center>
+<center>
 <h1> WELCOME, Hi <?php echo $_SESSION["UserID"];?> </h1>
-<p> Choose Your menu : </p>
+</center>
+  </div>
+</div>
+
+  <center>
+
+
 
 <?php 
     if($_SESSION["UserType"]=="Admin"){
@@ -46,7 +54,7 @@ if(isset($_SESSION["UserID"])){
     <?php 
     }
     ?>
-    <a href="logout.php">Logout </a><br>
+    
 </center>
 </body>
 </html>
@@ -62,12 +70,14 @@ else{
 </div>
 <script>
 function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
   document.getElementById("mySidebar").style.display = "block";
-  document.getElementById("myOverlay").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
 }
-
 function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
   document.getElementById("mySidebar").style.display = "none";
-  document.getElementById("myOverlay").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
 }
 </script>

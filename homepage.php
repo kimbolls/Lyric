c019ab9d6fc7275219cd71df8ba4da44224b4624
kimbolls <42145771+kimbolls@users.noreply.github.com>
2020-01-08 20:46:06 +0800

    Navigation bar on homepage
    navigation bar

--- homepage.php
+++ homepage.php
@@ -7,10 +7,25 @@ if(isset($_SESSION["UserID"])){
 
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <title> Hi-Fi - Music Registration </title>
 <body>
-<center>
-<br>
+
+<div class="w3-sidebar w3-bar-block w3-animate-left" style="display:none;z-index:5" id="mySidebar">
+  <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
+  <a href="music_insert.html" class="w3-bar-item w3-button"> Register New Music </a>
+  <a href="music_view.php" class="w3-bar-item w3-button"> View Music </a>
+  <a href="music_view.php" class="w3-bar-item w3-button" > View Music </a>
+  <a href="music_EditView.php" class="w3-bar-item w3-button"> Edit Music </a> 
+  <a href="music_DeleteView.php" class="w3-bar-item w3-button"> Delete Music </a>
+</div>
+
+<div class="w3-overlay w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" id="myOverlay"></div>
+<div>
+  <button class="w3-button w3-white w3-xxlarge" onclick="w3_open()">&#9776;</button>
+  <div class="w3-container">
+  <center>
 <h1> WELCOME, Hi <?php echo $_SESSION["UserID"];?> </h1>
 <p> Choose Your menu : </p>
 
@@ -18,15 +33,15 @@ if(isset($_SESSION["UserID"])){
     if($_SESSION["UserType"]=="Admin"){
         ?>
         <a href ="music_insert.html"> Register New Music </a> <br><br>
-}
+
 <?php 
     }
     else{
         ?>
-    <a href="music_insert.html"> Register New Music </a> <br><br>
-    <a href="music_view.php"> View Music </a><br><br>
-    <a href="music_EditView.php"> Edit Music </a> <br><br>
-    <a href="music_DeleteView.php"> Delete Music </a><br><br>
+    
+    
+    
+    
     
     <?php 
     }
@@ -41,5 +56,18 @@ else{
     echo "No session exists or session has expired. Please log in again. <br>";
     echo "<a href=loginmenu.html> Login </a>";
 }
-?>
 
+?>
+    </div>
+</div>
+<script>
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+</script>

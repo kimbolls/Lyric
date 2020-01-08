375ed07b0d6c7a3856ab545130ace3585947d5fe
kimbolls <42145771+kimbolls@users.noreply.github.com>
2020-01-08 20:25:26 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -24,8 +24,10 @@ if(isset($_SESSION["UserID"])){
     else{
         ?>
     <a href="music_insert.html"> Register New Music </a> <br><br>
+    <a href="music_view.php"> View Music </a><br><br>
     <a href="music_EditView.php"> Edit Music </a> <br><br>
     <a href="music_DeleteView.php"> Delete Music </a><br><br>
+    
     <?php 
     }
     ?>

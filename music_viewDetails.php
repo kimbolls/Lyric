806c21eb24a9d1c9dee220d0d874ec1a75a72644
kimbolls <42145771+kimbolls@users.noreply.github.com>
2020-01-09 13:59:09 +0800

    Notes for image uploading
    -view details
    -note txt

---
+++ music_viewDetails.php
@@ -0,0 +1,104 @@
+<?php 
+
+session_start();
+
+if(isset($_SESSION["UserID"])){
+    ?>
+
+<script src="music_script.js"></script>
+
+<html>
+
+<head><script src="music_script.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<title> Hi-Fi - Music Record </title>
+<body>
+<?php 
+    if($_SESSION["UserType"]=="Admin"){
+        ?>
+<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
+<button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
+<a href="music_insert.php" class="w3-bar-item w3-button"> Register New Music </a>
+<a href="logout.php" class="w3-bar-item w3-button">Logout </a><br>
+    </div>
+<?php 
+    }
+    else{
+        ?>
+    <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
+  <button class="w3-bar-item w3-button w3-large"
+  onclick="w3_close()">Close &times;</button>
+  <a href="homepage.php" class="w3-bar-item w3-button"> Home </a>
+  <a href="music_insert.php" class="w3-bar-item w3-button"> Register New Music </a>
+  <a href="music_view.php" class="w3-bar-item w3-button"> View Music </a>
+  <a href="music_EditView.php" class="w3-bar-item w3-button"> Edit Music </a> 
+  <a href="music_DeleteView.php" class="w3-bar-item w3-button"> Delete Music </a>
+  <a href="logout.php" class="w3-bar-item w3-button">Logout </a><br>
+</div>
+
+
+    <?php 
+    }
+    ?>
+<div id="main">
+
+<div class="w3-teal">
+  <button id="openNav" class="w3-button w3-teal w3-xlarge" onclick="w3_open()">&#9776;</button>
+  <div class="w3-container">
+  <h1>Music Details</h1>
+  </div>
+  </div>
+ <center>
+<?php 
+
+$songname = ;
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "hi-fi";
+
+$link = mysqli_connect($host,$user,$password,$database);
+
+if(!$link)
+{
+	die ("Could not connenct to database".mysqli_connect_error($link));
+}
+else
+{
+	$queryGet = "select * from music where Song_Name = '$songname' ";
+	$resultGet = mysqli_query($link,$queryGet);
+	
+	if(!$resultGet)
+	{
+		die ("Invalid Query - get Music list: ".mysqli_error($link));
+	}
+	else
+	{
+
+        while($baris = mysqli_fetch_assoc($resultGet))
+        {
+            ?>
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<?php
+        }
+    }
+  }
+}
+  else{
+    echo "No session exists or session has expired. Please log in again. <br>";
+    echo "<a href=loginmenu.html> Login </a>";
+  }
+  ?>
+  </body>
+  </html>

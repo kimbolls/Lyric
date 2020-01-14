33edd93ef73f3f823481eacefa5a3b8db81f0db4
Nik Asyraf <54460063+NikAsyraf454@users.noreply.github.com>
2020-01-14 22:42:57 +0800

    change of color theme
    chnage all edit and view file

--- music_EditAdmin.php
+++ music_EditAdmin.php
@@ -16,13 +16,14 @@ if(isset($_SESSION["UserID"])){
 <head><script src="music_script.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" type="text/css" href="style.css">
 <title> Hi-Fi - Music Record </title>
 <body>
 <?php 
     if($_SESSION["UserType"]=="Admin"){
         ?>
-<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
-<button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
+<div class="w3-sidebar w3-bar-block w3-card w3-animate-left secondcolor" style="display:none" id="mySidebar">
+<button class="w3-bar-item w3-button w3-large secondcolor" onclick="w3_close()">Close &times;</button>
 <a href="homepage.php" class="w3-bar-item w3-button"> Home </a>
 <a href="user_profile.php" class="w3-bar-item w3-button"> My Profile</a>
   <a href="music_view.php" class="w3-bar-item w3-button"> View Music </a>
@@ -33,8 +34,8 @@ if(isset($_SESSION["UserID"])){
     }
     else{
         ?>
-    <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
-  <button class="w3-bar-item w3-button w3-large"
+    <div class="w3-sidebar w3-bar-block w3-card w3-animate-left secondcolor" style="display:none" id="mySidebar">
+  <button class="w3-bar-item w3-button w3-large secondcolor"
   onclick="w3_close()">Close &times;</button>
   <a href="homepage.php" class="w3-bar-item w3-button"> Home </a>
   <a href="music_insert.php" class="w3-bar-item w3-button"> Register New Music </a>
@@ -50,8 +51,8 @@ if(isset($_SESSION["UserID"])){
     ?>
 <div id="main">
 
-<div class="w3-teal">
-  <button id="openNav" class="w3-button w3-teal w3-xlarge" onclick="w3_open()">&#9776;</button>
+<div class="maincolor">
+  <button id="openNav" class="w3-button maincolor w3-xlarge" onclick="w3_open()">&#9776;</button>
   <div class="w3-container">
   <h1>Music Record</h1>
   </div>
